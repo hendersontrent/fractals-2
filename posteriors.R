962a993819bc9c164ca95bc1ebbb385cc6fadb1a
Trent Henderson <trenthenderson@Trents-MacBook-Pro-2.local>
2021-03-04 23:49:34 +1100

    Switched to pure Stan

--- posteriors.R
+++ posteriors.R
@@ -83,50 +83,51 @@ df_t2_med <- time_cleaner(df_t2_med) %>%
 # Bind files together
 
 tmp <- bind_rows(df_t1_base, df_t1_med, df_t2_base, df_t2_med) %>%
-  mutate(state = as.factor(state)) %>%
-  mutate(condition = as.factor(condition))
+  mutate(state = ifelse(state == "Rest", 1, 2),
+         condition = ifelse(condition == "T1", 1, 2))
 
 #----------------------- Specify a Bayesian model ----------------------
 
 options(mc.cores = parallel::detectCores())
 
-# Specify priors
-
-b_condition_t2 <- paste0("normal(",priors$b_condition_t2_mean,",",priors$b_condition_t2_sd,")")
-b_state_rest <- paste0("normal(",priors$b_state_rest_mean,",",priors$b_state_rest_sd,")")
-b_minute <- paste0("normal(",priors$b_minute_mean,",",priors$b_minute_sd,")")
-b_condition_t2_state_rest <- paste0("normal(",priors$b_condition_t2_state_rest_mean,",",priors$b_condition_t2_state_rest_sd,")")
-b_condition_t2_minute <- paste0("normal(",priors$b_condition_t2_minute_mean,",",priors$b_condition_t2_minute_sd,")")
-b_state_rest_minute <- paste0("normal(",priors$b_state_rest_minute_mean,",",priors$b_state_rest_minute_sd,")")
-b_condition_t2_state_rest_minute <- paste0("normal(",priors$b_condition_t2_state_rest_minute_mean,",",priors$b_condition_t2_state_rest_minute_sd,")")
-
 # Fit model
 
-m1 <- brm(value ~ condition*state*minute,
-          prior = c(set_prior(prior = b_condition_t2, class = "b", coef = "conditionT2"),
-                    set_prior(prior = b_state_rest, class = "b", coef = "stateRest"),
-                    set_prior(prior = b_minute, class = "b", coef = "minute"),
-                    set_prior(prior = b_condition_t2_state_rest, class = "b", coef = "conditionT2:stateRest"),
-                    set_prior(prior = b_condition_t2_minute, class = "b", coef = "conditionT2:minute"),
-                    set_prior(prior = b_state_rest_minute, class = "b", coef = "stateRest:minute"),
-                    set_prior(prior = b_condition_t2_state_rest_minute, class = "b", coef = "conditionT2:stateRest:minute")),
-          data = tmp, iter = 2000, chains = 3, seed = 123)
+stan_data <- list(N = nrow(tmp),
+                  state = tmp$state,
+                  condition = tmp$condition,
+                  minute = tmp$minute,
+                  y = tmp$value,
+                  alpha_mean = priors$alpha_mean, alpha_sd = priors$alpha_sd,
+                  beta_1_mean = priors$beta_1_mean, beta_1_sd = priors$beta_1_sd,
+                  beta_2_mean = priors$beta_2_mean, beta_2_sd = priors$beta_2_sd,
+                  beta_3_mean = priors$beta_3_mean, beta_3_sd = priors$beta_3_sd,
+                  beta_4_mean = priors$beta_4_mean, beta_4_sd = priors$beta_4_sd,
+                  beta_5_mean = priors$beta_5_mean, beta_5_sd = priors$beta_5_sd,
+                  beta_6_mean = priors$beta_6_mean, beta_6_sd = priors$beta_6_sd,
+                  beta_7_mean = priors$beta_7_mean, beta_7_sd = priors$beta_7_sd)
+
+m2 <- stan(file = "stan/fractal2.stan",
+           data = stan_data, iter = 2000, chains = 3, seed = 123)
           
 #--------------------- Compute outputs & data vis ----------------------
 
 # Diagnostic 1: Chain convergence
 
-mcmc_trace(m1)
+mcmc_trace(m2)
 
 # Diagnostic 2: LOO
 
-loo1 <- loo(m1, save_psis = TRUE)
+loo1 <- loo(m2, save_psis = TRUE)
 plot(loo1)
 
 # Diagnostic 3: Posterior predictive checks
 
 CairoPNG("output/fractals_2_PPC.png",800,600)
-pp_check(m1, nsamples = 100) +
+set.seed(123)
+y <- tmp$value
+yrep <- extract(m2)[["y_rep"]]
+samp100 <- sample(nrow(yrep), 100)
+ppc_dens_overlay(y, yrep[samp100, ]) +
   labs(title = "Posterior predictive check",
        x = "HRV",
        y = "Count")
@@ -135,9 +136,8 @@ dev.off()
 # Summative data visualisation
 
 CairoPNG("output/fractals_2_posterior.png",800,600)
-mcmc_areas(m1, regex_pars = c("conditionT2", "stateRest", "minute",
-                                  "conditionT2:stateRest", "conditionT2:minute", "stateRest:minute",
-                                  "conditionT2:stateRest:minute"),
+mcmc_areas(m2, regex_pars = c("beta_1", "beta_2", "beta_3", 
+                              "beta_4", "beta_5", "beta_6", "beta_7"),
            area_method = "scaled height") +
   labs(title = "Coefficient posterior distributions")
 dev.off()
